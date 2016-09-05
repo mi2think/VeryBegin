3b87d327a540ce3d5b4c37ccef7543939b18e955
Wenbiao Hou <mi2think@gmail.com>
2016-09-05 23:58:19 +0800

    Add DX9 Demo 01 - Init

---
+++ 3D/DirectX/DX9/Demo/01_DX9Init/main.cpp
@@ -0,0 +1,28 @@
+#include "sims.h"
+#include "core/log.h"
+#include "graphics_api/sims_dx9.h"
+#include "utils/demo_app.h"
+using namespace sims;
+
+class Dx9Init : public DemoApp<dx9::Window>
+{
+public:
+	virtual void OnRender(const Timestep& timestep)
+	{
+		LOG_INFO("ts:%d", timestep.GetMilliseconds());
+		if (dx9::g_pD3DD)
+		{
+			dx9::g_pD3DD->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xff0000ff, 1.0f, 0);
+			dx9::g_pD3DD->Present(0, 0, 0, 0);
+		}
+	}
+};
+
+int main()
+{
+	Dx9Init app;
+	app.Create(640, 480, "Dx9 Init");
+	app.Loop();
+
+	return 0;
+}
