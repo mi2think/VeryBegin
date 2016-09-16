9f5cf7eb7804e8ae1fc704976863c21e84c208d4
Wenbiao Hou <mi2think@gmail.com>
2016-09-16 19:34:04 +0800

    Add DX9 Demo 24 - Progressive Mesh

---
+++ 3D/DirectX/DX9/Demo/24_ProgressiveMesh/main.cpp
@@ -0,0 +1,216 @@
+#include "sims.h"
+#include "core/log.h"
+#include "core/key_event.h"
+#include "graphics_api/sims_dx9.h"
+#include "graphics_api/sims_dx9_geometry_def.h"
+#include "utils/demo_app.h"
+using namespace sims;
+
+class ProgressiveMesh : public DemoApp<dx9::Window>
+{
+public:
+	ProgressiveMesh()
+		: mesh_(nullptr)
+		, pmesh_(nullptr)
+	{}
+
+	virtual void OnCreate()
+	{
+		DemoApp<dx9::Window>::OnCreate();
+
+		// load .x file
+		ID3DXBuffer* adjBuffer = nullptr;
+		ID3DXBuffer* mtrlBuffer = nullptr;
+		DWORD numMtrls = 0;
+
+		dx9::CHECK_HR = D3DXLoadMeshFromX("bigship1.x",
+			D3DXMESH_MANAGED,
+			dx9::g_pD3DD,
+			&adjBuffer,
+			&mtrlBuffer,
+			0,
+			&numMtrls,
+			&mesh_);
+
+		// extract the materials, and load textures
+		if (mtrlBuffer != nullptr && numMtrls > 0)
+		{
+			D3DXMATERIAL* mt = (D3DXMATERIAL*)mtrlBuffer->GetBufferPointer();
+			for (int i = 0; i < numMtrls; ++i)
+			{
+				D3DMATERIAL9& matD3D = mt[i].MatD3D;
+
+				// the MatD3D property does not have an ambient value set when its loaded, so set it now
+				matD3D.Ambient = matD3D.Diffuse;
+
+				// save the material
+				mtrls_.push_back(matD3D);
+
+				// check if the material has an associative texture
+				if (mt[i].pTextureFilename != nullptr)
+				{
+					// load texture
+					IDirect3DTexture9* tex = nullptr;
+					dx9::CHECK_HR = D3DXCreateTextureFromFile(dx9::g_pD3DD,
+						mt[i].pTextureFilename,
+						&tex);
+					texs_.push_back(tex);
+				}
+				else
+				{
+					texs_.push_back(nullptr);
+				}
+			}
+		}
+		SAFE_RELEASE(mtrlBuffer);
+
+		DWORD* adj = (DWORD*)adjBuffer->GetBufferPointer();
+
+		// optimize mesh
+		dx9::CHECK_HR = mesh_->OptimizeInplace(D3DXMESHOPT_ATTRSORT | D3DXMESHOPT_COMPACT,
+			adj,
+			adj,  // must need!!!
+			0,
+			0);
+		dx9::CHECK_HR = D3DXValidMesh(mesh_, adj, nullptr);
+		// generate progressive mesh
+		dx9::CHECK_HR = D3DXGeneratePMesh(mesh_,
+			(DWORD*)adjBuffer->GetBufferPointer(),
+			0,
+			0,
+			1,
+			D3DXMESHSIMP_FACE,
+			&pmesh_);
+
+		SAFE_RELEASE(adjBuffer);
+
+		// set origin max face
+		pmesh_->SetNumFaces(pmesh_->GetMaxFaces());
+
+		int a = pmesh_->GetMinFaces();
+		int b = pmesh_->GetMaxFaces();
+
+		// set texture filter
+		dx9::CHECK_HR = dx9::g_pD3DD->SetSamplerState(0, D3DSAMP_MAGFILTER, D3DTEXF_LINEAR);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetSamplerState(0, D3DSAMP_MINFILTER, D3DTEXF_LINEAR);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetSamplerState(0, D3DSAMP_MIPFILTER, D3DTEXF_LINEAR);
+
+		// setup directional light
+		D3DLIGHT9 light;
+		memset(&light, 0, sizeof(light));
+		light.Type = D3DLIGHT_DIRECTIONAL;
+		light.Diffuse = white;
+		light.Specular = white;
+		light.Ambient = white;
+		light.Direction = D3DXVECTOR3(1.0f, -1.0f, 1.0f);
+
+		dx9::CHECK_HR = dx9::g_pD3DD->SetLight(0, &light);
+		dx9::CHECK_HR = dx9::g_pD3DD->LightEnable(0, true);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetRenderState(D3DRS_LIGHTING, true);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetRenderState(D3DRS_SPECULARENABLE, true);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetRenderState(D3DRS_NORMALIZENORMALS, true);
+
+		// view
+		D3DXVECTOR3 pos(8.0f, 4.0f, -12.0f);
+		D3DXVECTOR3 target(0.0f, 0.0f, 0.0f);
+		D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);
+		D3DXMATRIX view;
+		D3DXMatrixLookAtLH(&view, &pos, &target, &up);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetTransform(D3DTS_VIEW, &view);
+
+		// projection
+		D3DXMATRIX proj;
+		D3DXMatrixPerspectiveFovLH(&proj,
+			D3DX_PI * 0.5f,
+			(float)width_ / height_,
+			1.0f,
+			1000.0f);
+		dx9::CHECK_HR = dx9::g_pD3DD->SetTransform(D3DTS_PROJECTION, &proj);
+	}
+
+	virtual void OnRender(const Timestep& timestep)
+	{
+		if (dx9::g_pD3DD)
+		{
+			// draw
+			dx9::CHECK_HR = dx9::g_pD3DD->Clear(0, 0, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, 0xffffffff, 1.0f, 0);
+			dx9::CHECK_HR = dx9::g_pD3DD->BeginScene();
+
+			for (int i = 0; i < mtrls_.size(); ++i)
+			{
+				// draw pmesh
+				auto& mtrl = mtrls_[i];
+				dx9::CHECK_HR = dx9::g_pD3DD->SetMaterial(&mtrl);
+				dx9::CHECK_HR = dx9::g_pD3DD->SetTexture(0, texs_[i]);
+				dx9::CHECK_HR = pmesh_->DrawSubset(i);
+
+				// draw wireframe outline
+				D3DMATERIAL9 yellowMtrl;
+				GenMaterial(yellowMtrl, yellow, yellow, yellow, black, 2.0f);
+				dx9::CHECK_HR = dx9::g_pD3DD->SetMaterial(&yellowMtrl);
+				dx9::CHECK_HR = dx9::g_pD3DD->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);
+				dx9::CHECK_HR = pmesh_->DrawSubset(i);
+				dx9::CHECK_HR = dx9::g_pD3DD->SetRenderState(D3DRS_FILLMODE, D3DFILL_SOLID);
+			}
+
+			dx9::CHECK_HR = dx9::g_pD3DD->EndScene();
+			dx9::CHECK_HR = dx9::g_pD3DD->Present(0, 0, 0, 0);
+		}
+	}
+
+	virtual bool OnEvent(const Event& event)
+	{
+		EventDispatch dispatch(event);
+		dispatch.Dispatch(this, &ProgressiveMesh::OnKeyPressEvent);
+		return dispatch.GetResult();
+	}
+
+	bool OnKeyPressEvent(const KeyPressEvent& event)
+	{
+		int key = event.GetKey();
+		int numFaves = pmesh_->GetNumFaces();
+
+		if (key == 'A')
+		{
+			// add a face, note SetNumFace will automatically clamp value
+
+			// sometimes we must add more than one face to invert an edge collapse transformation
+			pmesh_->SetNumFaces(numFaves + 10);
+			if (pmesh_->GetNumFaces() == numFaves)
+				pmesh_->SetNumFaces(numFaves + 12);
+		}
+		else if (key == 'S')
+		{
+			// remove a face
+			pmesh_->SetNumFaces(numFaves - 10);
+		}
+
+		LOG_INFO("faces:%d", numFaves);
+		return true;
+	}
+
+	virtual void OnDestroy()
+	{
+		SAFE_RELEASE(mesh_);
+		SAFE_RELEASE(pmesh_);
+		for (auto& tex : texs_)
+		{
+			SAFE_RELEASE(tex);
+		}
+	}
+
+
+private:
+	ID3DXMesh* mesh_;
+	ID3DXPMesh* pmesh_;
+	vector<D3DMATERIAL9> mtrls_;
+	vector<IDirect3DTexture9*> texs_;
+};
+
+int main()
+{
+	ProgressiveMesh app;
+	app.Create(640, 480, "Progressive Mesh");
+	app.Loop();
+	return 0;
+}
